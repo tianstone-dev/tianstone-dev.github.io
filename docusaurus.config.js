da25c8591142278d66f7f02c4da70eafb8efc0fa
tianstone2022 <tian0stone2022@outlook.com>
2024-10-19 11:49:03 +0800

    modified:   docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/tianstone-dev/tianstone-dev.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/tianstone-dev/tianstone-dev.github.io/tree/develop/',
         },
         blog: {
           showReadingTime: true,
